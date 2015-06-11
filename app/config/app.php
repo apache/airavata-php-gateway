887eed2fb1fae10ff1d48b8043fd293be21d0464
Supun Nakandala <scnakandala@apache.org>
2015-06-11 23:15:25 +0530

    removing not used dependency

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,6 @@ return array(
         'Illuminate\Validation\ValidationServiceProvider',
         'Illuminate\View\ViewServiceProvider',
         'Illuminate\Workbench\WorkbenchServiceProvider',
-        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
         'Wsis\WsisServiceProvider',
         'Airavata\AiravataServiceProvider',
     ),
