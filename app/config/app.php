46104a4129cbbaca097e6c4afced1695412836df
Nipurn Doshi <nipunhere@gmail.com>
2015-09-10 15:31:42 -0400

    Theming addition.

--- app/config/app.php
+++ app/config/app.php
@@ -123,6 +123,7 @@ return array(
         'Illuminate\Workbench\WorkbenchServiceProvider',
         'Wsis\WsisServiceProvider',
         'Airavata\AiravataServiceProvider',
+        'Teepluss\Theme\ThemeServiceProvider',
     ),
 
     /*
@@ -191,6 +192,7 @@ return array(
         'View' => 'Illuminate\Support\Facades\View',
         'WSIS' => 'Wsis\Facades\Wsis',
         'Airavata' => 'Airavata\Facades\Airavata',
+        'Theme' => 'Teepluss\Theme\Facades\Theme',
     ),
 
 );
