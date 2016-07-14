9c1af936652cabbc6f416ad5d8e044c2f25d67a4
Suresh Marru <smarru@apache.org>
2016-07-14 16:14:56 -0400

    Updating the tutorial to XSEDE16

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -28,8 +28,8 @@ if( Session::get("theme") == "base"){?>
         </p>
         <p><a href="https://github.com/apache/airavata-php-gateway"
                 target="_blank">See the code <span class="glyphicon glyphicon-new-window"></span></a></p>
-        <p><a href="https://cwiki.apache.org/confluence/display/AIRAVATA/XSEDE+2015+Tutorial"
-            target="_blank">XSEDE 2015 tutorial documentation <span class="glyphicon glyphicon-new-window"></span></a>
+        <p><a href="https://cwiki.apache.org/confluence/display/AIRAVATA/XSEDE+2016+Tutorials"
+            target="_blank">XSEDE 2016 tutorial documentation <span class="glyphicon glyphicon-new-window"></span></a>
         </p>
     </div>
 </div>
