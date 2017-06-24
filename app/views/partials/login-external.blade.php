f6cc00abd12b0e2ee041cbbf742658f4175c3e76
Marcus Christie <machrist@iu.edu>
2017-06-24 14:15:18 -0400

    AIRAVATA-2430 Logo config option for external identity providers

--- app/views/partials/login-external.blade.php
+++ app/views/partials/login-external.blade.php
@@ -1,4 +1,9 @@
 
 @foreach ($auth_code_options as $auth_code_option)
-    <a href="{{ $auth_code_option["auth_url"] }}" class="btn btn-primary">Sign in with {{{ $auth_code_option["name"] }}}</a>
+    <a href="{{ $auth_code_option["auth_url"] }}" class="btn btn-primary">
+        @if (isset($auth_code_option["logo"]))
+        <img src="{{ $auth_code_option["logo"] }}">
+        @endif
+        Sign in with {{{ $auth_code_option["name"] }}}
+    </a>
 @endforeach
