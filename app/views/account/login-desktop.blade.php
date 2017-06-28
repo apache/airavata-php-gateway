37e096e84142066b891bb0bb6e23cda402fceb85
scnakandala <supun.nakandala@gmail.com>
2017-06-28 16:24:10 -0400

    size changes

--- app/views/account/login-desktop.blade.php
+++ app/views/account/login-desktop.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 
-<div class="col-md-offset-5 col-md-2">
+<div class="col-md-offset-4 col-md-4">
     @if (!empty($auth_password_option))
         @include('partials/login-form', array("auth_name" => $auth_password_option["name"], "desktop" => true))
         @if (!empty($auth_code_options))
