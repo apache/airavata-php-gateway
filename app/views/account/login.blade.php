fd6675b85ea6f193b476c85b65c43330c52a3b25
Marcus Christie <machrist@iu.edu>
2017-11-03 11:56:31 -0400

    AIRAVATA-2502 External logins listed first (on the left)

--- app/views/account/login.blade.php
+++ app/views/account/login.blade.php
@@ -6,17 +6,34 @@
 
 @section('content')
 
-<div class="col-md-offset-4 col-md-4">
+@if(count($auth_code_options) > 0)
+    @if($has_auth_code_and_password_options)
+    <div class="col-md-offset-1 col-md-4 center-column">
+    @else
+    <div class="col-md-offset-4 col-md-4 center-column">
+    @endif
 
-    @if (!empty($auth_password_option))
-        @include('partials/login-form', array("auth_name" => $auth_password_option["name"]))
-        @if (!empty($auth_code_options))
-            <h3 id="login-option-separator" class="horizontal-rule">OR</h4>
-        @endif
+    @foreach ($auth_code_options as $auth_code_option)
+        @include('partials/login-external', array("auth_code_option" => $auth_code_option))
+    @endforeach
+    </div>
+@endif
+
+@if($has_auth_code_and_password_options)
+    <div class="col-md-2 center-column">
+        <h3 id="login-option-separator" class="horizontal-rule">OR</h3>
+    </div>
+@endif
+
+@if(!empty($auth_password_option))
+    @if($has_auth_code_and_password_options)
+    <div class="col-md-4 center-column">
+    @else
+    <div class="col-md-offset-4 col-md-4 center-column">
     @endif
-    @if (!empty($auth_code_options))
-        @include('partials/login-external', array("auth_code_options" => $auth_code_options))
-    @endif
-</div>
+
+    @include('partials/login-form', array("auth_name" => $auth_password_option["name"]))
+    </div>
+@endif
 
 @stop
